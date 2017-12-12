5a5720b29f3dbcb215f34e8d66d04b0451f757f7
Sushma Wavemaker <sushma.maddirala@wavemaker.com>
2017-12-12 09:51:44 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,14 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "zxddsdsd",
+  "homePage" : "Main",
+  "name" : "zxddsdsd",
+  "platformType" : "WEB",
+  "securityEnabled" : "true",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
